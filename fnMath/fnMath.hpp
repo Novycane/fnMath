c5e0d30ad4edb853f640465589d59d841cb98540
Steven Novak <stevenrnovak@gmail.com>
2017-02-01 13:29:27 -0800

    Initial Commit

---
+++ fnMath/fnMath.hpp
@@ -0,0 +1,23 @@
+/*
+ *  fnMath.hpp
+ *  fnMath
+ *
+ *  Created by Steven Novak on 2/1/17.
+ *  Copyright © 2017 Steven Novak. All rights reserved.
+ *
+ */
+
+#ifndef fnMath_
+#define fnMath_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class fnMath
+{
+    public:
+    void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
---
+++ fnMath/fnMathPriv.hpp
@@ -0,0 +1,19 @@
+/*
+ *  fnMathPriv.hpp
+ *  fnMath
+ *
+ *  Created by Steven Novak on 2/1/17.
+ *  Copyright © 2017 Steven Novak. All rights reserved.
+ *
+ */
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class fnMathPriv
+{
+    public:
+    void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
