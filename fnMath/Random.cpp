4a42d7b5679e6eeab336a797eff9bd6648748bbf
Steven Novak <stevenrnovak@gmail.com>
2017-02-07 11:33:34 -0800

    Initial Commit

---
+++ fnMath/Random.cpp
@@ -0,0 +1,9 @@
+//
+//  Random.cpp
+//  fnMath
+//
+//  Created by Steven Novak on 2/7/17.
+//  Copyright © 2017 Steven Novak. All rights reserved.
+//
+
+#include "Random.hpp"
---
+++ fnMath/Random.hpp
@@ -0,0 +1,28 @@
+//
+//  Random.hpp
+//  fnMath
+//
+//  Created by Steven Novak on 2/7/17.
+//  Copyright © 2017 Steven Novak. All rights reserved.
+//
+
+#ifndef Random_hpp
+#define Random_hpp
+
+namespace fnMath{
+    
+class Random
+{
+public:
+    
+    
+private:
+    bool hardwareRNGPresent;
+    
+    void checkHardwareAvailability();
+    
+}; // End Random
+
+}
+
+#endif /* Random_hpp */
