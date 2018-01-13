755ad4097d99ee7c603985d57f39d15a342fbf17
Steven Novak <snovak@incomm.com>
2018-01-12 16:23:19 -0800

    Added Crypto directory

---
+++ test/Random.test.cpp
@@ -0,0 +1,24 @@
+//
+//  Random.test.cpp
+//  
+//
+//  Created by Steven Novak on 12/6/17.
+//
+// --------------------------------------------------
+//
+//
+//
+//
+// --------------------------------------------------
+
+#include <iostream>
+#include "../src/Stats/Random.hpp"
+
+using namespace fnMath;
+
+int main (int argCount, char** args)
+{   
+    std::cout << "Hello world Yo\n" << std::endl;
+    
+    return 1;
+}
