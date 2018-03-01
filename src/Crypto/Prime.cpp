10d78ca7e965ca2e3685bb4107ff3d6a9d13b96a
Steven Novak <snovak@incomm.com>
2018-03-01 08:50:08 -0800

    Added tests for prime number generation. Added improvements to not double check even nubmers

--- src/Crypto/Prime.cpp
+++ src/Crypto/Prime.cpp
@@ -29,16 +29,40 @@ unsigned long long Prime::GetPrime(unsigned long long maxNumber)
 
 unsigned long long Prime::Segmented(unsigned long long maxNumber)
 {
-    // Guess the number of primes in sqrt(maxNumber)
-    // Allocate that memory
-    // Sieve to sqrt(n)
+    double sqMax = sqrt(maxNumber);
+    
+    std::cout << "Max Number: " << maxNumber << std::endl;
+    std::cout << "Initial Sieve: " << sqMax << std::endl;
+    std::cout << "Number of segments: " << (maxNumber / sqMax) << std::endl;
 
+    // Allocate that memory
+    unsigned long long* primes = (unsigned long long*)malloc(sqMax * sizeof(unsigned long long));
+
+    // Sieve to sqrt(n)
+    bool* flags = (bool*)malloc(sqMax * sizeof(bool));
+    int k=0;
+    for(int i=0; i<sqMax; i++)
+        flags[i] = true;
+    for(unsigned long long i=2; i<sqMax; i++)
+    {
+        if(flags[i] == true)
+        {
+            primes[k++] = i;    
+            for(unsigned long long p = i + i; p<sqMax; p+=i)
+                flags[p] = false;
+        }
+    }
+    delete flags;
 
     // Divide the total range into segments of size < sqrt(n)
+    
+
     // for each segment of size x...
     // -- Create an array to store flags
     // -- Iterate through the primes in the initial sieve
     // -- Mark it's multiples in the segment
+
+    delete primes;
 
     return 0;
 }
@@ -66,20 +90,25 @@ unsigned long long Prime::Naive (unsigned long long maxNumber)
     return result;
 }
 
-unsigned long long NaiveSQ (unsigned long long maxNumber)
+
+unsigned long long Prime::NaiveSQ (unsigned long long maxNumber)
 {
 bool* flags = (bool*)malloc(maxNumber * sizeof(bool));
 
     for(int i=0; i<maxNumber; i++)
-        flags[i] = true;
+        if(i & 1) 
+            flags[i] = true;
+        else
+            flags[i] = false;
  
     unsigned long long result;
-    for(unsigned long long i=2; i< sqrt(maxNumber); i++)
+    
+    for(unsigned long long i=3; i < maxNumber; i++)
     {
         if(flags[i] == true)
         {
             result = i; 
-            for(unsigned long long p = i + i; p<maxNumber; p+=i)
+            for(unsigned long long p = i + 2 * i; p<maxNumber; p += 2 * i)
             {
                 flags[p] = false;
             }
--- test/Prime.test.cpp
+++ test/Prime.test.cpp
@@ -18,39 +18,24 @@
 using namespace fnMath;
 using namespace Crypto;
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-bool testNaivePrime();
-bool testNaiveSQPrime();
-
-int main (int argCount, char** args)
-{
-    testNaivePrime();
-    testNaiveSQPrime();
-}
-
-bool testNaivePrime()
-{
-    std::cout << "Testing Naive Prime Number Generator\n" << std::endl;
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 bool TestPrime();
+bool TestSegmentedPrime();
+bool TestSQPrime();
+void ComparePerformance();
 void PrintTime(clock_t now);
 
 int main (int argCount, char** args)
 {
     TestPrime();
+    TestSQPrime();
+    ComparePerformance();
+    TestSegmentedPrime();
 }
 
 bool TestPrime()
 {
     std::cout << "Testing Prime Number Generator\n" << std::endl;
     
->>>>>>> Stashed changes
     Prime myPrime;
     clock_t  now;
 
@@ -86,7 +71,7 @@ bool TestPrime()
     PrintTime(now);
     if(result != 99991)
     {
-        std::cout << "Highest Prime < 10,000 Found To Be: " << result << " : Expected 9,973 " << std::endl;
+        std::cout << "Highest Prime < 100,000 Found To Be: " << result << " : Expected 99,991 " << std::endl;
         return false;
     }
 
@@ -96,7 +81,7 @@ bool TestPrime()
     PrintTime(now);
     if(result != 999983)
     {
-        std::cout << "Highest Prime < 10,000 Found To Be: " << result << " : Expected 9,973 " << std::endl;
+        std::cout << "Highest Prime < 1,000,000 Found To Be: " << result << " : Expected 999,983 " << std::endl;
         return false;
     }
 
@@ -105,31 +90,22 @@ bool TestPrime()
     PrintTime(now);
     if(result != 9999991)
     {
-        std::cout << "Highest Prime < 10,000 Found To Be: " << result << " : Expected 9,973 " << std::endl;
+        std::cout << "Highest Prime < 10,000,000 Found To Be: " << result << " : Expected 9,999,991 " << std::endl;
         return false;
     }
-
 
     return true;
 }
 
-<<<<<<< Updated upstream
-bool testNaiveSQPrime()
+bool TestSQPrime()
 {
-    std::cout << "Testing NaiveSQ Prime Number Generator\n" << std::endl;
+    std::cout << "Testing Odd Only Prime Number Generator\n" << std::endl;
+    
     Prime myPrime;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    clock_t now, later;
-    unsigned long long maxNumber = 10000000001;
-    now = clock();
-    unsigned long long result = myPrime.Naive(maxNumber);
-    later = clock();
-=======
     clock_t  now;
 
     now = clock();
-    unsigned long long result = myPrime.GetPrime(20);
+    unsigned long long result = myPrime.NaiveSQ(20);
     PrintTime(now);
     if(result != 19)
     {
@@ -138,7 +114,7 @@ bool testNaiveSQPrime()
     }
 
     now = clock();
-    result = myPrime.GetPrime(100);
+    result = myPrime.NaiveSQ(100);
     PrintTime(now);
     if(result != 97)
     {
@@ -147,7 +123,7 @@ bool testNaiveSQPrime()
     }
 
     now = clock();
-    result = myPrime.GetPrime(10000);
+    result = myPrime.NaiveSQ(10000);
     PrintTime(now);
     if(result != 9973)
     {
@@ -156,115 +132,94 @@ bool testNaiveSQPrime()
     }
 
     now = clock();
-    result = myPrime.GetPrime(100000);
+    result = myPrime.NaiveSQ(100000);
     PrintTime(now);
     if(result != 99991)
     {
-        std::cout << "Highest Prime < 10,000 Found To Be: " << result << " : Expected 9,973 " << std::endl;
+        std::cout << "Highest Prime < 100,000 Found To Be: " << result << " : Expected 99,991 " << std::endl;
         return false;
     }
 
 
     now = clock();
-    result = myPrime.GetPrime(1000000);
+    result = myPrime.NaiveSQ(1000000);
     PrintTime(now);
     if(result != 999983)
     {
-        std::cout << "Highest Prime < 10,000 Found To Be: " << result << " : Expected 9,973 " << std::endl;
+        std::cout << "Highest Prime < 1,000,000 Found To Be: " << result << " : Expected 999,983 " << std::endl;
         return false;
     }
 
     now = clock();
-    result = myPrime.GetPrime(10000000);
+    result = myPrime.NaiveSQ(10000000);
     PrintTime(now);
     if(result != 9999991)
     {
-        std::cout << "Highest Prime < 10,000 Found To Be: " << result << " : Expected 9,973 " << std::endl;
+        std::cout << "Highest Prime < 10,000,000 Found To Be: " << result << " : Expected 9,999,991 " << std::endl;
         return false;
     }
->>>>>>> Stashed changes
 
-
-<<<<<<< Updated upstream
-    return 0;
-=======
-=======
     return true;
 }
 
->>>>>>> Stashed changes
-=======
+void ComparePerformance()
+{
+    std::cout << "Comparing Performance\n" << std::endl;
+    Prime myPrime;
+    clock_t  now;
+    unsigned long long result;
+
+    now = clock();
+    result = myPrime.Naive(3000000000);
+    PrintTime(now);
+    std::cout << "Odd And Even " << result << std::endl;
+
+    now = clock();
+    result = myPrime.NaiveSQ(3000000000);
+    PrintTime(now);
+    std::cout << "Odd Only " << result << std::endl;
+
+}
+
+bool TestSegmentedPrime()
+{
+    std::cout << "Testing Segmented Prime Number Generator\n" << std::endl;
+    
+    Prime myPrime;
     clock_t  now;
 
     now = clock();
-    unsigned long long result = myPrime.GetPrime(20);
+    unsigned long long result = myPrime.Segmented(20);
     PrintTime(now);
     if(result != 19)
     {
         std::cout << "Highest Prime < 20 Found To Be: " << result << " : Expected 19 " << std::endl;
-        return false;
+        //return false;
     }
 
     now = clock();
-    result = myPrime.GetPrime(100);
+    result = myPrime.Segmented(100);
     PrintTime(now);
     if(result != 97)
     {
         std::cout << "Highest Prime < 100 Found To Be: " << result << " : Expected 97 " << std::endl;
-        return false;
+        //return false;
     }
 
     now = clock();
-    result = myPrime.GetPrime(10000);
+    result = myPrime.Segmented(10000);
     PrintTime(now);
     if(result != 9973)
     {
         std::cout << "Highest Prime < 10,000 Found To Be: " << result << " : Expected 9,973 " << std::endl;
-        return false;
+        //return false;
     }
-
-    now = clock();
-    result = myPrime.GetPrime(100000);
-    PrintTime(now);
-    if(result != 99991)
-    {
-        std::cout << "Highest Prime < 10,000 Found To Be: " << result << " : Expected 9,973 " << std::endl;
-        return false;
-    }
-
-
-    now = clock();
-    result = myPrime.GetPrime(1000000);
-    PrintTime(now);
-    if(result != 999983)
-    {
-        std::cout << "Highest Prime < 10,000 Found To Be: " << result << " : Expected 9,973 " << std::endl;
-        return false;
-    }
-
-    now = clock();
-    result = myPrime.GetPrime(10000000);
-    PrintTime(now);
-    if(result != 9999991)
-    {
-        std::cout << "Highest Prime < 10,000 Found To Be: " << result << " : Expected 9,973 " << std::endl;
-        return false;
-    }
-
 
     return true;
 }
 
->>>>>>> Stashed changes
 void PrintTime(clock_t now)
 {
     clock_t later = clock();
     std::cout << ((float)(later - now) / CLOCKS_PER_SEC) << " s elapsed" << std::endl;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 }
