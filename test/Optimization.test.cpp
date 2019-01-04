412c0e532907c402d3292a856b3cce7c84f14add
Novycane <stevenrnovak@gmail.com>
2019-01-03 17:05:03 -0800

    Updated to partial derivatives

--- test/Optimization.test.cpp
+++ test/Optimization.test.cpp
@@ -13,13 +13,31 @@
 
 #include <iostream>
 #include <cmath>
+#include "../src/fnMath.hpp"
 #include "../src/Optimization/GradientDescent.hpp"
 
 using namespace fnMath;
 using namespace Optimization;
+using namespace std;
 
 // ------------------------- Declarations
 bool TestGradientDescent();
+
+// ------------------------- Functions
+
+class Quadratic : public fnMath::Function
+{
+public:
+    double Evaluate(double x)
+    {
+        return x * x - 5;
+    }
+
+    double Evaluate(fnMath::LinAlg::MatrixD params)
+    {
+        return params[0][0] * params[0][0] - 5;
+    }
+};
 
 // ------------------------- Main
 
@@ -34,6 +52,13 @@ int main (int argCount, char** args)
 bool TestGradientDescent()
 {
 	std::cout << "Testing Gradient Descent Functions" << std::endl;
+	vector<bool> flags;
+	GradientDescent GD; 
+	
+	flags.push_back(true);
+	
+	GD.SetFunction(new Quadratic());
+	GD.SetFlags(flags);
 	
 	return true;
 }
